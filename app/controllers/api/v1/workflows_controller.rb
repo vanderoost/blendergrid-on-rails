79da837b9a3761b30e76226703682232b4cd3580
Richard van der Oost <richard@vanderoost.com>
2025-05-04 12:17:29 +0100

    Put API in a namespace

--- app/controllers/api/v1/workflows_controller.rb
+++ app/controllers/api/v1/workflows_controller.rb
@@ -1,14 +1,11 @@
-class WorkflowsController < ApplicationController
+class Api::V1::WorkflowsController < ApplicationController
   include ::ActionController::HttpAuthentication::Token::ControllerMethods
 
-  # TOOD: Use an API namespace for "update"
   allow_unauthenticated_access # To turn off email/password login
   skip_before_action :verify_authenticity_token # To turn off CSRF protection
   before_action :authenticate, :set_workflow
 
   def update
-    Rails.logger.info "Update Workflow - Params: #{params.inspect}"
-
     if params[:status] == "finished"
       @workflow.finalize(result: params[:result])
     end
@@ -21,15 +18,11 @@ class WorkflowsController < ApplicationController
   private
     def authenticate
       authenticate_or_request_with_http_token do |token, options|
-        Rails.logger.info "Authenticating with token: '#{token}'"
         token == Rails.application.credentials.api_token
       end
     end
 
-    # Use callbacks to share common setup or constraints between actions.
     def set_workflow
-      Rails.logger.info "Setting workflow #{params[:id]}"
       @workflow = Workflow.find_by(uuid: params[:id])
-      Rails.logger.info "Found workflow #{@workflow.inspect}"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # API
+  namespace :api do
+    namespace :v1 do
+      resources :workflows, only: [ :update ]
+    end
+  end
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
@@ -15,7 +22,6 @@ Rails.application.routes.draw do
   resources :project_sources
   resources :projects
   resources :registrations, only: [ :new, :create ]
-  resources :workflows, only: [ :update ]
 
   root "home#index"
 end
