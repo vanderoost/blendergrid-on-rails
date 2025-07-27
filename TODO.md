a85fbc62bd27d6a31da88c96826208bb942e08c5
Richard van der Oost <richard@vanderoost.com>
2025-07-27 11:45:01 +0100

    Add ECR lifecycle policy

---
+++ TODO.md
@@ -0,0 +1,26 @@
+# TODOs
+
+## essentials
+
+- [ ]  Emails
+    - [ ]  Capture email address of guests
+    - [ ]  Send project status emails
+    - [ ]  Send sign-up confirmation email
+    - [ ]  Send password reset emails
+- [ ]  Edit project settings
+    - [ ]  Between first check and price quote
+    - [ ]  Between price quote and render
+- [ ]  Support >20GB uploads through the website
+- [ ]  Use an external database (AWS RDS? Mysql / Postgres)
+- [ ]  UI/UX
+    - [ ]  Pay once for multiple projects
+    - [ ]  Show simple render progress and ETA
+    - [ ]  Basic style so it doesn’t look like a 90’s primary school website
+- [ ]  Transfer over articles and affiliate landing pages
+- [x]  Clean up old ECR images
+
+## nice-to-haves
+
+- [ ]  Really nice styling with Tailwind
+- [ ]  Drag ‘n Drop uploads
+- [ ]  Track visits and affiliate clicks in the backend
--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,44 @@
 resource "aws_ecr_repository" "web_app" {
   name = "blendergrid/web-app"
 }
+
+resource "aws_ecr_lifecycle_policy" "web_app" {
+  repository = aws_ecr_repository.web_app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep the 16 most recent untagged images, expire all others"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "imageCountMoreThan"
+          countNumber = 16
+        }
+        action = { type = "expire" }
+      },
+      {
+        rulePriority = 2
+        description  = "Keep the 8 most recent tagged images, expire all others"
+        selection = {
+          tagStatus      = "tagged"
+          tagPatternList = ["*"],
+          countType      = "imageCountMoreThan"
+          countNumber    = 8
+        }
+        action = { type = "expire" }
+      },
+      {
+        rulePriority = 3
+        description  = "Expire all images older than 30 days"
+        selection = {
+          tagStatus   = "any"
+          countType   = "sinceImagePushed",
+          countUnit   = "days",
+          countNumber = 30
+        }
+        action = { type = "expire" }
+      },
+    ]
+  })
+}
