8201a42424874d75c2546d3a928b5632d62d9b35
Richard van der Oost <richard@vanderoost.com>
2025-08-03 22:11:16 +0100

    Add percentage and ETA summary

--- app/javascript/controllers/direct_uploads_controller.js
+++ app/javascript/controllers/direct_uploads_controller.js
@@ -1,33 +1,23 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["list"]
+  static targets = ["list", "summary"]
 
   connect() {
-    this.bound = {
-      initUpload: this.initUpload.bind(this),
-      progress: this.progress.bind(this),
-      error: this.error.bind(this),
-      end: this.end.bind(this),
-    }
-
-    this.element.addEventListener("direct-upload:initialize", this.bound.initUpload)
-    this.element.addEventListener("direct-upload:progress", this.bound.progress)
-    this.element.addEventListener("direct-upload:error", this.bound.error)
-    this.element.addEventListener("direct-upload:end", this.bound.end)
-
     this.items = new Map()
+    this.totalSize = 0
+    this.uploadedSizes = new Map()
+    this.smoothEta = null
+    this.etaCalcDelay = 3000
   }
 
-  disconnect() {
-    this.element.removeEventListener("direct-upload:initialize", this.bound.initUpload)
-    this.element.removeEventListener("direct-upload:progress", this.bound.progress)
-    this.element.removeEventListener("direct-upload:error", this.bound.error)
-    this.element.removeEventListener("direct-upload:end", this.bound.end)
+  uploadsStart() {
+    this.startTime = Date.now()
   }
 
-  initUpload(e) {
+  uploadInit(e) {
     const { id, file } = e.detail
+    this.totalSize += file.size
     const wrapper = document.createElement("div")
     wrapper.className = "upload-progress"
     wrapper.dataset.uploadId = id
@@ -39,13 +29,41 @@ export default class extends Controller {
     this.items.set(id, wrapper)
   }
 
-  progress(e) {
-    const { id, progress } = e.detail
+  uploadProgress(e) {
+    const { file, id, progress } = e.detail
+    this.uploadedSizes.set(id, Math.round(file.size * progress / 100))
     const el = this.items.get(id)
     if (el) el.querySelector("[data-progress]").value = progress
+
+    this.trackTotalProgress()
   }
 
-  error(e) {
+  trackTotalProgress() {
+    const bytesDone = this.uploadedSizes.values().reduce((acc, n) => acc + n, 0)
+    const percent = bytesDone / this.totalSize * 100
+
+    const now = Date.now()
+    const elapsed = now - this.startTime
+
+    let progressMessage = `Uploading ${percent.toFixed(1)}%`
+
+    if (elapsed > this.etaCalcDelay) {
+      const bytesRemaining = this.totalSize - bytesDone
+      const eta = now + elapsed / bytesDone * bytesRemaining
+
+      if (this.smoothEta === null) {
+        this.smoothEta = eta
+      } else {
+        this.smoothEta = 0.99 * this.smoothEta + 0.01 * eta
+      }
+      const remaining = this.smoothEta - Date.now()
+      progressMessage += ` - ${humanDuration(remaining)} remaining`
+    }
+
+    this.summaryTarget.innerHTML = progressMessage
+  }
+
+  uploadError(e) {
     const { id, error } = e.detail
     const el = this.items.get(id)
     if (el) {
@@ -55,7 +73,7 @@ export default class extends Controller {
     }
   }
 
-  end(e) {
+  uploadEnd(e) {
     const { id } = e.detail
     const el = this.items.get(id)
     if (el) {
@@ -64,4 +82,29 @@ export default class extends Controller {
       this.items.delete(id)
     }
   }
+}
+
+function humanDuration(ms) {
+  let seconds = Math.ceil(ms / 1000)
+  let minutes = Math.floor(seconds / 60)
+  let hours = Math.floor(minutes / 60)
+  let days = Math.floor(hours / 24)
+
+  if (days > 1) {
+    return `${days} days`
+  }
+
+  if (hours > 1) {
+    return `${hours} hours`
+  }
+
+  if (minutes > 1) {
+    return `${minutes} minutes`
+  }
+
+  if (seconds > 1) {
+    return `${seconds} seconds`
+  }
+
+  return "1 second"
 }
