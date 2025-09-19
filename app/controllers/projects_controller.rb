0946c184126aaaade1bc7d3b6dca470637a921bf
Richard van der Oost <richard@vanderoost.com>
2025-09-19 21:19:09 +0100

    Design a Project Settings edit page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,6 @@
 class ProjectsController < ApplicationController
-  before_action :set_project, only: :show
-  allow_unauthenticated_access only: %i[ index show ]
+  before_action :set_project, only: %i[ show edit ]
+  allow_unauthenticated_access only: %i[ index show edit ]
 
   def index
     # @projects = Project.joins(:upload).merge(accessible_uploads)
@@ -10,7 +10,19 @@ class ProjectsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    @project.update(project_params)
+    redirect_to @project
+  end
+
   private
+    def project_params
+      params.require(:project).permit(:name, :description, :uuid)
+    end
+
     def set_project
       @project = Project.find_by!(uuid: params[:uuid])
     end
