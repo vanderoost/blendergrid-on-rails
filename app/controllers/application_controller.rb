0ca996c296a66ef5db98f2ba28f5ec1510c986bd
Richard van der Oost <richard@vanderoost.com>
2025-10-24 14:27:21 +0100

    Grab all session uploads on login and assign them to the user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,6 @@ class ApplicationController < ActionController::Base
   include Authentication
   include RequestTracking
 
-  # TODO: Consider moving these to a concern
-  def accessible_uploads
-    authenticated? ? Current.user.uploads : current_guest_uploads
-  end
-
-  def current_guest_uploads
-    return Upload.none unless session[:guest_email_address] # TODO: Can we skip this?
-    Upload.where(
-      guest_email_address: session[:guest_email_address],
-      guest_session_id: session.id.to_s,
-      user_id: nil
-    )
-  end
-
   private
     def redirect_to_safe_url(url)
       return redirect_to projects_path if url.blank?
