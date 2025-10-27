529b55c9d07b5b77606046e946b6f5847f0dafa8
Richard van der Oost <richard@vanderoost.com>
2025-10-27 09:41:08 +0000

    Fix rubocop and global Gem shenanigans
    Install all gems locally in the project folder instead of globally.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_10_13_152555) do
+ActiveRecord::Schema[8.2].define(version: 2025_10_13_152555) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -121,7 +121,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_10_13_152555) do
     t.datetime "created_at", null: false
     t.string "guest_email_address"
     t.string "guest_session_id"
-    t.string "receipt_url"
+    t.string "stripe_payment_intent_id"
+    t.string "stripe_receipt_url"
     t.string "stripe_session_id"
     t.datetime "updated_at", null: false
     t.integer "user_id"
