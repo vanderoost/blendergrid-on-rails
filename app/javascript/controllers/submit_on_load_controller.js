8cb44eac61cfbdb20c1337a3808fef6480526e4d
Richard van der Oost <richard@vanderoost.com>
2026-06-03 17:13:09 +0100

    Cleanup

--- app/javascript/controllers/submit_on_load_controller.js
+++ app/javascript/controllers/submit_on_load_controller.js
@@ -1,9 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Submits the form it's attached to as soon as it connects. Used to make the
-// marketing unsubscribe link one-click: the GET landing page auto-submits the
-// unsubscribe form. Link prefetchers / scanners that don't run JS never fire it,
-// and the visible button is the no-JS fallback.
 export default class extends Controller {
   connect() {
     this.element.requestSubmit()
