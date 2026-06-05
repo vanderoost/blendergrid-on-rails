5e52f06f6fe5bed10fb7074051d0b39c17ce4d99
Richard van der Oost <richard@vanderoost.com>
2026-06-05 13:15:05 +0100

    Cleanup migrations

--- db/migrate/20260605110856_create_subscribers.rb
+++ db/migrate/20260605110856_create_subscribers.rb
@@ -2,18 +2,14 @@ class CreateSubscribers < ActiveRecord::Migration[8.2]
   def change
     create_table :subscribers do |t|
       t.string :name
-      t.string :guest_email_address
-      t.references :user, foreign_key: true, index: false
-      t.datetime :deleted_at
+      t.string :guest_email_address,
+        index: { unique: true, where: "user_id IS NULL" }
+      t.references :user, foreign_key: true,
+        index: { unique: true, where: "user_id IS NOT NULL" }
+      t.datetime :deleted_at, index: true
       t.string :source
 
       t.timestamps
     end
-
-    # A user has at most one subscriber; a guest email is unique among guest rows.
-    add_index :subscribers, :user_id, unique: true, where: "user_id IS NOT NULL"
-    add_index :subscribers, :guest_email_address,
-      unique: true, where: "user_id IS NULL"
-    add_index :subscribers, :deleted_at
   end
 end
